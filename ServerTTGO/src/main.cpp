924ead730d0c90fbee94386b055a36ff05747983
PauloValim <paulo.valim@gmail.com>
2021-12-18 18:14:03 -0300

    Segundo exemplo, apenas o esqueleto para teste do git

---
+++ ServerTTGO/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <RHSoftwareSPI.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
